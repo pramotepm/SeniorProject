b25552ca64786b7df488c5ca18c4fe08d22e38f1
Pramote Teerasetmanakul <boatblaster@gmail.com>
2013-12-10 23:14:47 +0700

    just save

--- src/net/mikelab/webbase/link/HTMLTagWithAnchorParse.java
+++ src/net/mikelab/webbase/link/HTMLTagWithAnchorParse.java
@@ -1,25 +1,46 @@
 package net.mikelab.webbase.link;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.mikelab.webbase.struct.Link;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonWriter;
+
 public class HTMLTagWithAnchorParse {
-	private Set<String> destinationURL = new HashSet<>();
 	private String filePath;
-	private String lineSeperator = "==<<==>>==SEPERATOR==<<==>>==";
+	private List<Link> LinkURLs = new LinkedList<Link>();
 	
 	public HTMLTagWithAnchorParse(String filePath) {
-		// TODO Auto-generated constructor stub
 		this.filePath = filePath; 
+	}
+	
+	public void writeToFile(String filePath) throws IOException {
+		Gson g = new GsonBuilder().setPrettyPrinting().create();
+		Path p = Paths.get(filePath);
+		BufferedWriter w = Files.newBufferedWriter(p, StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+		JsonWriter jw = new JsonWriter(w);
+		jw.setSerializeNulls(true);
+		w.write(g.toJson(LinkURLs));
+		w.close();
+		jw.close();
 	}
 	
 	public void extractAllLink() {
@@ -28,21 +49,21 @@ public class HTMLTagWithAnchorParse {
 			while((line = br.readLine()) != null) {
 				if (line.startsWith("URL: ") && !line.endsWith("robots.txt")) {
 					String URL = line.replaceFirst("URL: ", "");
+					Link LinkURL = new Link(URL);
 					String html = br.readLine();
 					Document doc = Jsoup.parseBodyFragment(html, URL);
 					for (Element e : doc.select("a[href]")) {
+						String anchorText = new String(e.text().getBytes(), "UTF-8");
 						String absURLPath = e.attr("abs:href");
 						if (absURLPath != null && absURLPath.startsWith("http")) {
-							destinationURL.add(absURLPath);
+							LinkURL.add(absURLPath, anchorText);
 						}
 					}
-					for (String s : destinationURL)
-						System.out.println(s);
-					System.out.println(lineSeperator);
+					LinkURLs.add(LinkURL);
 				}
 			}
+			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}  
 	}
